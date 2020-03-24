43ad13dbe6a4b8f5abe5bc72953fb27c5b7a8c61
Yuvraj Jain <jainy3@mcmaster.ca>
2020-03-24 21:13:13 +0000

    Submitting Project02

---
+++ Project02/project2.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+function disableButton(btn){
+	document.getElementById(btn.id).disabled = true;
+	}
+
+$(document).ready(function(){
+  	$("#star").click(function(){
+    	$("#rock").hide(1000);
+ 		});
+});
+document.getElementById("myButton").onclick = function () {
+    location.href = "https://www.linkedin.com/in/yuvraj-jain-compsci/";
+};
+document.getElementById("myButton2").onclick = function () {
+    location.href = "https://www.instagram.com/_yuvraj_jain/";
+};
+});
