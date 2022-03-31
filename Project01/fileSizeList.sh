b9df98aaa7d951d1cfb8ff9770e5e11a50ac87cd
yuvraj-jain-2002 <101608375+yuvraj-jain-2002@users.noreply.github.com>
2022-03-30 23:04:11 -0400

    Delete Project01 directory

--- Project01/fileSizeList.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-fileList=$(ls -lS)
-echo "$(ls -lS | sort -k 5 -rn | du -ah | sort -hr)"
--- Project01/fileTypeCount.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Enter the file extension you want to search (without the period):"
-read extension
-ls *."$extension" | wc -l
